16da3c5595434adccc6dc59b736167a71d82fdbe
Brouks <joshua.waters365@gmail.com>
2025-07-11 15:11:38 -0400

    Added sentinal NIL Valtype IKVValtype::NIL

--- src/kvobj.cpp
+++ src/kvobj.cpp
@@ -2,21 +2,24 @@
 #include "kvt_map.h"
 #include "kvt_string.h"
 #include "kvt_tset.h"
+
 #include <memory>
 
 namespace redbrouk
 {
 
-KVObj::KVObj(KVTYPE _type) : type(_type) {
+IKVValtype IKVValtype::NIL;
+
+KVObj::KVObj(KVTYPE _type) : m_type(_type) {
 	switch(_type) {
 		case KVTYPE::STRING:
-			val = std::make_unique<String>();
+			m_val = std::make_unique<String>();
 			break;
 		case KVTYPE::HASH:
-			val = std::make_unique<iHMap>();
+			m_val = std::make_unique<iHMap>();
 			break;
 		case KVTYPE::TSET:
-			val = std::make_unique<TSet>();
+			m_val = std::make_unique<TSet>();
 			break;
 		default:
 			break;
