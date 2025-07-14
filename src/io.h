39af1b297dcbed262629863730cb3f40411f9f7e
Brouks <joshua.waters365@gmail.com>
2025-07-14 13:32:14 -0400

    Added sending and receiving buffers to io_context and changed parse_req to take reference to vector of string views instead of strings

--- src/io.h
+++ src/io.h
@@ -12,6 +12,8 @@ namespace redbrouk
 {
 
 using socket_t = int;
+using std::byte;
+using sview = std::string_view;
 
 class Conn;
 
@@ -37,12 +39,16 @@ typedef struct io_context {
 
 	bool running = true;
 	socket_t highFd = -1;
+
+	byte recv_buf[1024 * 4];
+	byte send_buf[1024 * 4];
 } ioc; // struct io_context
 
 struct Response {
     uint32_t status = 0;
     std::vector<uint8_t> data;
 };
+
 enum {
     RES_OK = 0,
     RES_ERR = 1,
@@ -53,7 +59,7 @@ static void handle_read(Conn *conn);
 static void handle_write(Conn *conn);
 static void do_request(std::vector<std::string> &cmd, Response &out);
 static int  try_request(Conn *conn);
-static int32_t parse_req(const std::byte*, size_t, std::vector<std::string>&);
+static int32_t parse_req(const std::byte*, size_t, std::vector<std::string_view>&);
 
 void sigint_handler(int sig_num);
 } // namespace redbrouk
