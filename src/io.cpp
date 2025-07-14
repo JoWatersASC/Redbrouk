983b8d2f1c7d1b05bf6afec875029bd6fe9121b0
Brouks <joshua.waters365@gmail.com>
2025-07-14 13:33:44 -0400

    Formatting changes, changed command functions to take references to vectors of string views instead of vectors of strings, functions use in_data() and ot_data() functions instead of manually computing offsets for connection buffers

--- src/io.cpp
+++ src/io.cpp
@@ -61,7 +61,7 @@ void io_context::main_loop() {
 			if(!conn)
 				continue;
 
-			struct pollfd pfd = { conn->get_socket(), POLLERR, 0 };
+			struct pollfd pfd{ conn->get_socket(), POLLERR, 0 };
 
 			if((bool)(conn->state & ConnState::RECVING))
 				pfd.events |= POLLIN;
@@ -89,6 +89,8 @@ void io_context::main_loop() {
 
 				assert(!connections[fd]);
 				connections[fd] = conn;
+				conn->in_buff = recv_buf;
+				conn->ot_buff = send_buf;
 			}
 		}
 
@@ -99,14 +101,14 @@ void io_context::main_loop() {
 				continue;
 
 			Conn *conn = connections[pfds[i].fd];
-            if (ready & POLLIN) {
-                assert(conn->state & ConnState::RECVING);
-                handle_read(conn);  // application logic
-            }
-            if (ready & POLLOUT) {
-                assert(conn->state & ConnState::SENDING);
-                handle_write(conn); // application logic
-            }
+			if (ready & POLLIN) {
+				assert(conn->state & ConnState::RECVING);
+				handle_read(conn);  // application logic
+			}
+			if (ready & POLLOUT) {
+				assert(conn->state & ConnState::SENDING);
+				handle_write(conn); // application logic
+			}
 
 			if((ready & POLLERR) || (bool)(conn->state & ConnState::CLOSED)) {
 				conn->Close();
@@ -119,60 +121,56 @@ void io_context::main_loop() {
 
 static void handle_read(Conn *conn) {
 	std::byte buf[64 * 1024];
-    ssize_t rv = conn->recv(buf, sizeof(buf));
+	ssize_t rv = conn->recv(buf, sizeof(buf));
 
-    if (rv < 0 && errno == EAGAIN) {
-        return;
-    }
+	if (rv < 0 && errno == EAGAIN)
+		return;
 
-    if (rv < 0) {
-        conn->state = ConnState::CLOSED | ConnState::ERR;
+	if (rv < 0) {
+		conn->state = ConnState::CLOSED | ConnState::ERR;
 		std::println("[ERROR] handle_read: {}", strerror(errno));
-        return;
-    }
+		return;
+	}
 
-    // handle EOF
-    if (rv == 0) {
-        if (conn->in_start == conn->in_end) {
+	// handle EOF
+	if (rv == 0) {
+		if (conn->in_start == conn->in_end) {
 			std::println("client closed");
-        } else {
+		} else {
 			std::println("unexpected EOF");
-        }
+		}
 
-        conn->state = ConnState::CLOSED;
-        return; // want close
-    }
+		conn->state = ConnState::CLOSED;
+		return; // want close
+	}
 
-	memcpy(conn->in_buff + conn->in_start, buf, (size_t)rv);
-	// conn->in_start += (size_t)rv;
+	memcpy(conn->in_data(), buf, (size_t)rv);
 
-    while (try_request(conn));
+	while (try_request(conn));
 
-    if (conn->ot_size() > 0) {    // has a response
-        conn->state &= ~ConnState::RECVING;
-        conn->state |=  ConnState::SENDING;
+	if (conn->ot_size() > 0) {    // has a response
+		conn->state &= ~ConnState::RECVING;
+		conn->state |=  ConnState::SENDING;
 
-        return handle_write(conn);
-    }
+		return handle_write(conn);
+	}
 }
 static void handle_write(Conn *conn) {
-	ssize_t rv = conn->send(conn->ot_buff + conn->ot_start, conn->ot_end - conn->ot_start);
+	ssize_t rv = conn->send(conn->ot_data(), conn->ot_size());
 
-	if (rv < 0 && errno == EAGAIN) {
-        return;
-    }
-    if (rv < 0) {
-        conn->state = ConnState::CLOSED | ConnState::ERR;
+	if (rv < 0 && errno == EAGAIN)
+		return;
+
+	if (rv < 0) {
+		conn->state = ConnState::CLOSED | ConnState::ERR;
 		std::println("[ERROR] handle_read: {}", strerror(errno));
-        return;
-    }
-	
-	conn->ot_start += rv;
+		return;
+	}
 
-    if (conn->ot_start == conn->ot_end) {   // all data written
-        conn->state &= ~ConnState::SENDING;
-        conn->state |=  ConnState::RECVING;
-    }
+	if (conn->ot_start == conn->ot_end) {   // all data written
+		conn->state &= ~ConnState::SENDING;
+		conn->state |=  ConnState::RECVING;
+	}
 }
 
 using std::vector;
@@ -199,10 +197,115 @@ namespace {
 	}
 }
 
+static void do_request(std::vector<sview> &cmd, Response &out) {
+	using req_func = void(std::vector<sview>&, Response&);
+	req_func get_val, set_val, del_val,
+			do_add_tset, do_range_tset;
+
+	const size_t cmd_len = cmd.size();
+
+	if (cmd_len == 2 && cmd[0] == "get") {
+		get_val(cmd, out);
+		if(out.status == RES_NX)
+			return;
+
+	} else if (cmd_len == 3 && cmd[0] == "set") {
+		set_val(cmd, out);
+	} else if (cmd_len == 2 && cmd[0] == "del") {
+		del_val(cmd, out);
+	} else if (cmd_len >= 4 && cmd[0] == "tadd") {
+		do_add_tset(cmd, out);
+	} else if (cmd_len == 4 && cmd[0] == "trange") {
+		do_range_tset(cmd, out);
+	} else {
+		out.status = RES_ERR;
+	}
+}
+
+namespace {
+	static void make_response(const Response &res, byte *out, off_t &offset) {
+		uint32_t rlen = 4 + (uint32_t)res.data.size();
+
+		memcpy(out + offset, (byte *)&rlen, sizeof(rlen));
+		offset += sizeof(rlen);
+		memcpy(out + offset, (byte *)&res.status, sizeof(res.status));
+		offset += sizeof(res.status);
+		memcpy(out + offset, (byte *)res.data.data(), res.data.size());
+		offset += res.data.size();
+	}
+}
+
+static int try_request(Conn *conn) {
+	if(conn->in_size() < 4)
+		return false;
+
+	uint32_t len = 0;
+	memcpy(&len, conn->in_data(), sizeof(len));
+	if(len > 20000) {
+		std::println("too long");
+		conn->state = ConnState::CLOSED;
+
+		return false;
+	}
+
+	if(len + sizeof(len) > conn->in_size())
+		return false;
+
+	const byte *request = conn->in_data() + 4;
+
+	std::vector<sview> cmd;
+	if(parse_req(request, len, cmd) < 0) {
+		std::println("Bad request");
+		conn->state = ConnState::CLOSED;
+
+		return false;
+	}
+
+	Response res;
+	do_request(cmd, res);
+	make_response(res, conn->ot_buff, conn->ot_end);
+
+	conn->in_start += 4 + len;
+	return true;
+}
+
+static int32_t parse_req(const byte *data, size_t len, std::vector<sview>& out) {
+	const byte *end = data + len;
+	uint32_t nstr = 0;
+
+	if(end - data < 4)
+		return -1;
+
+	memcpy(&nstr, data, sizeof(nstr));
+	data += 4;
+
+	if(nstr > 20000)
+		return -1;
+
+	while(out.size() < nstr) {
+		uint32_t str_len = 0;
+		if(end - data < sizeof(str_len))
+			return -1;
+		memcpy(&str_len, data, sizeof(str_len));
+		data += sizeof(str_len);
+
+		if(end - data < str_len)
+			return -1;
+
+		out.emplace_back((const char *)data, str_len);
+		data += str_len;
+	}
+
+	if(data != end)
+		return -1;
+
+	return 0;
+}
+
 //---------------------------------------------------------------------------------------
 // String val type functions
 //---------------------------------------------------------------------------------------
-void get_val(vector<string> &cmds, Response &out) { // get command from cmds vector
+void get_val(vector<sview> &cmds, Response &out) { // get command from cmds vector
 	LookupDummy dummy{
 		.hook = { nullptr, genHash((const byte *)cmds[1].data(), cmds[1].length()) },
 		.key  = cmds[1]
@@ -222,12 +325,12 @@ void get_val(vector<string> &cmds, Response &out) { // get command from cmds vec
 		res = "[ERROR: TYPE_MM] Was expecting STRING type";
 		out.data.assign(res.begin(), res.end());
 	} else {
-		res = "[GET] Key: " + std::move(cmds[1]) + " Val: " + (String&)container.val();
+		res = "[GET] Key: " + std::string(cmds[1]) + " Val: " + (String&)container.val();
 	}
 
 	out.data.assign(res.begin(), res.end());
 }
-void set_val(vector<string> &cmds, Response &out) {
+void set_val(vector<sview> &cmds, Response &out) {
 	LookupDummy dummy{
 		.hook = { nullptr, genHash((const byte *)cmds[1].data(), cmds[1].length()) },
 		.key  = cmds[1]
@@ -253,7 +356,7 @@ void set_val(vector<string> &cmds, Response &out) {
 	out.data.assign(res.begin(), res.end());
 	out.status = RES_OK;
 }
-void del_val(vector<string> &cmds, Response &out) {
+void del_val(vector<sview> &cmds, Response &out) {
 	LookupDummy dummy{
 		.hook = { nullptr, genHash((const byte*)cmds[1].data(), cmds[1].length()) },
 		.key  = cmds[1]
@@ -274,7 +377,7 @@ void del_val(vector<string> &cmds, Response &out) {
 		res = "[ERROR: TYPE_MM] Was expecting STRING type";
 		out.status = RES_ERR;
 	} else {
-		res = "[DEL] Key: " + container.get_key() + "Value: " + (String&)container.val();
+		res = "[DEL] Key: " + container.get_key() + " Val: " + (String&)container.val();
 	}
 
 	out.data.assign(res.begin(), res.end());
@@ -304,7 +407,7 @@ TSet *find_tset(std::string_view key) {
 
 	return std::addressof((TSet&)container.val());
 }
-void do_range_tset(vector<string> &cmds, Response &out) {
+void do_range_tset(vector<sview> &cmds, Response &out) {
 	std::string res = "[ERROR: TYPE_MM] Was expecting TSET type";
 	TSet *tset = find_tset(cmds[1]);
 
@@ -314,14 +417,16 @@ void do_range_tset(vector<string> &cmds, Response &out) {
 		return;
 	}
 	if(tset == &NILTSET) {
-		res = "[NULL] No TSet object with key " + cmds[1];
+		res = "[NULL] No TSet object with key " + std::string(cmds[1]);
 		out.data.assign(res.begin(), res.end());
 		out.status = RES_NX;
 		return;
 	}
 	res.clear();
 
-	ssize_t begin = std::stol(cmds[2]), end = std::stol(cmds[3]), tsize = ts_size(tset);
+	ssize_t begin = atoi((const char *)cmds[2].data()),
+	        end   = atoi((const char *)cmds[3].data()),
+	        tsize = ts_size(tset);
 	if(end < 0)
 		end = tsize + end;
 	if(end >= tsize || end < 0) {
@@ -341,8 +446,7 @@ void do_range_tset(vector<string> &cmds, Response &out) {
 	out.status = RES_OK;
 }
 
-// Inputs - 1) # of member/score pairs 2-4) length of member string, member string, score (repeating for $1 times)
-void do_add_tset(vector<string> &cmds, Response &out) {
+void do_add_tset(vector<sview> &cmds, Response &out) {
 	std::string res = "[ERROR: TYPE_MM] Was expecting TSET type";
 	const size_t size = cmds.size() - 1;
 	TSet *tset = find_tset(cmds[1]);
@@ -359,13 +463,13 @@ void do_add_tset(vector<string> &cmds, Response &out) {
 	double _score;
 	size_t inserted = 0, updated = 0;
 	for(size_t i = 2; i < size; i++) {
-		_score = strtod(cmds[i + 1].c_str(), nullptr);
+		_score = strtod(cmds[i + 1].data(), nullptr);
 
 		if( TSTNode *found = ts_find(tset, cmds[i]) ) {
 			ts_update(tset, found, _score);
 			updated++;
 		} else {
-			ts_insertn(tset, cmds[i], _score);
+			ts_insertn(tset, std::string(cmds[i]), _score);
 			inserted++;
 		}
 		i++;
@@ -374,108 +478,6 @@ void do_add_tset(vector<string> &cmds, Response &out) {
 	res = fmt("[TADD] Inserted: {}, Updated: {}", inserted, updated);
 	out.data.assign(res.begin(), res.end());
 	out.status = RES_OK;
-}
-
-static void do_request(std::vector<std::string> &cmd, Response &out) {
-	const size_t cmd_len = cmd.size();
-
-    if (cmd_len == 2 && cmd[0] == "get") {
-		get_val(cmd, out);
-		if(out.status == RES_NX)
-			return;
-
-    } else if (cmd_len == 3 && cmd[0] == "set") {
-		set_val(cmd, out);
-    } else if (cmd_len == 2 && cmd[0] == "del") {
-		del_val(cmd, out);
-    } else if (cmd_len > 3 && cmd[0] == "tadd") {
-		do_add_tset(cmd, out);
-	} else if (cmd_len == 4 && cmd[0] == "trange") {
-		do_range_tset(cmd, out);
-	} else {
-        out.status = RES_ERR;
-    }
-}
-
-namespace {
-	static void make_response(const Response &res, byte *out, off_t &offset) {
-		uint32_t rlen = 4 + (uint32_t)res.data.size();
-
-		memcpy(out + offset, (byte *)&rlen, sizeof(rlen));
-		offset += sizeof(rlen);
-		memcpy(out + offset, (byte *)&res.status, sizeof(res.status));
-		offset += sizeof(res.status);
-		memcpy(out + offset, (byte *)res.data.data(), res.data.size());
-		offset += res.data.size();
-	}
-}
-
-static int try_request(Conn *conn) {
-	if(conn->in_size() < 4)
-		return false;
-
-	uint32_t len = 0;
-	memcpy(&len, conn->in_buff, sizeof(len));
-	if(len > 20000) {
-		std::println("too long");
-		conn->state = ConnState::CLOSED;
-
-		return false;
-	}
-
-	if(4 + len > conn->in_size())
-		return false;
-
-	const byte *request = conn->in_buff + 4;
-
-	std::vector<std::string> cmd;
-	if(parse_req(request, len, cmd) < 0) {
-		std::println("Bad request");
-		conn->state = ConnState::CLOSED;
-
-		return false;
-	}
-
-	Response res;
-	do_request(cmd, res);
-	make_response(res, conn->ot_buff, conn->ot_end);
-
-	conn->in_start += 4 + len;
-	return true;
-}
-
-static int32_t parse_req(const byte *data, size_t len, std::vector<std::string>& out) {
-	const byte *end = data + len;
-	uint32_t nstr = 0;
-
-	if(end - data < 4)
-		return -1;
-
-	memcpy(&nstr, data, sizeof(nstr));
-	data += 4;
-
-	if(nstr > 20000)
-		return -1;
-
-	while(out.size() < nstr) {
-		uint32_t str_len = 0;
-		if(end - data < sizeof(str_len))
-			return -1;
-		memcpy(&str_len, data, sizeof(str_len));
-		data += sizeof(str_len);
-
-		out.push_back(std::string());
-		if(end - data < str_len)
-			return -1;
-
-		out.back().assign(data, data + str_len);
-		data += str_len;
-	}
-
-	if(data != end)
-		return -1;
-
-	return 0;
 }
 
 } // namespace redbrouk
